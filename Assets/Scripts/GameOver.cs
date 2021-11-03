5a6e50943a561ad82b122a01cb241a7f1872c192
Cassidy Barr <90427280+Jinx2498@users.noreply.github.com>
2021-11-03 19:48:12 -0400

    some changes

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameOver : MonoBehaviour
+{
+    public TextMeshProUGUI gameOverText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void gameOver() {
+        gameOverText.gameObject.SetActive(true);
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
